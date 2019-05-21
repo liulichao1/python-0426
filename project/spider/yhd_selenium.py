be30586168cb85c67edf66219779eb29598097bb
liulichao1 <2273360936@qq.com>
2019-05-21 18:52:44 +0800

    Update

---
+++ project/spider/yhd_selenium.py
@@ -0,0 +1,9 @@
+# @Time : 2019/5/17 8:41
+# @Author : 2273360936@qq.com
+# @FileName : yhd_selenium.py
+# @GitHub : https://github.com/liulichao1/python-0426
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.yhd.com/")
---
+++ 机器学习/__init__.py
@@ -0,0 +1,4 @@
+# @Time : 2019/5/21 10:47
+# @Author : 2273360936@qq.com
+# @FileName : __init__.py.py
+# @GitHub : https://github.com/liulichao1/python-0426
